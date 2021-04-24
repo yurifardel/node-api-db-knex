465a6c89a037c428527fafc1f900895463a34a4c
yurifardel <yrnascimento@outlook.com>
2021-04-23 21:14:48 -0300

    has what finish

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const PeopleController = require("../lib/controllers/CreatePeople");
 const PageController = require("../lib/controllers/PageController");
+const IdController = require("../lib/controllers/IdController");
 
 const routes = express.Router();
 
@@ -10,5 +11,7 @@ routes.get("/pessoa", PeopleController.index);
 
 routes.post("/pagina", PageController.create);
 routes.get("/pagina", PageController.index);
+
+routes.get("/pessoa-id", IdController.index);
 
 module.exports = routes;
