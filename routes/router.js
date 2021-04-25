fb4c5c72f5492fca0cd2d6a69df792cf3da14ac2
yurifardel <yrnascimento@outlook.com>
2021-04-25 01:57:37 -0300

    failed migration

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const PeopleController = require("../lib/controllers/CreatePeople");
 const PageController = require("../lib/controllers/PageController");
-const IdController = require("../lib/controllers/IdController");
 
 const routes = express.Router();
 
@@ -11,7 +10,5 @@ routes.get("/pessoa", PeopleController.index);
 
 routes.post("/pagina", PageController.create);
 routes.get("/pagina", PageController.index);
-
-routes.get("/pessoa-id", IdController.index);
 
 module.exports = routes;
